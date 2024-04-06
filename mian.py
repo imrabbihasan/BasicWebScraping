5829c40ef1c91d9085c0259995ee18a32f16283a
imrabbihasan <rabbihossain270@gmail.com>
2024-04-06 16:29:59 +0800

    Update in main code: - Find user's name - Find Number of followers - Find user's bio - Find user's location - Find user's email - Find user's number of repositories

--- mian.py
+++ mian.py
@@ -9,10 +9,40 @@ try:
     r.raise_for_status()  # Raise an exception for the HTTP errors
     soup = Bs(r.content, 'html.parser')
 
-    #  Find the profile image using a more general selector
-    profile_image = soup.find('img', class_='avatar')['src']
+    # Find the user's name
+    name = soup.find('span', class_='p-name')
+    name = name.text if name else 'Not available'
+    print('Name: ', name)
 
-    print('Profile image URL: ', profile_image)
+    # Number of followers
+    followers = soup.find_all('span', class_='Counter')
+    followers = followers[1].text if followers else 'Not available'
+    print('Followers: ', followers)
+
+    # Find the user's bio
+    bio = soup.find('div', class_='p-note')
+    bio = bio.text if bio else 'Not available'
+    print('Bio: ', bio)
+
+    # Find the user's location
+    location = soup.find('span', class_='p-label')
+    location = location.text if location else 'Not available'
+    print('Location: ', location)
+
+    # Find the user's email
+    email = soup.find('a', class_='u-email')
+    email = email.text if email else 'Not available'
+    print('Email: ', email)
+
+    #  Find the profile image using a more general selector
+    profile_image = soup.find('img', class_='avatar')
+    profile_image = profile_image['src'] if profile_image else 'Not available'
+    print('Profile Image: ', profile_image)
+
+    # Find the number of repositories
+    repo = soup.find_all('span', class_='Counter')
+    repos = repo[0].text if repo else 'Not available'
+    print('Number of repositories: ', repos)
 
 except requests.exceptions.RequestException as e:
     print('Error fetching data:', e)

1b3c970c5ad117906f00e33e4fc39ff7a4215b1e
imrabbihasan <rabbihossain270@gmail.com>
2024-04-06 18:12:54 +0800

    Update in the main code: - Generate the user's GitHub profile link

--- mian.py
+++ mian.py
@@ -9,6 +9,10 @@ try:
     r.raise_for_status()  # Raise an exception for the HTTP errors
     soup = Bs(r.content, 'html.parser')
 
+    # Generate the user's GitHub profile link
+    profile_link = url
+    print('Profile Link: ', profile_link)
+
     # Find the user's name
     name = soup.find('span', class_='p-name')
     name = name.text if name else 'Not available'
@@ -23,6 +27,11 @@ try:
     bio = soup.find('div', class_='p-note')
     bio = bio.text if bio else 'Not available'
     print('Bio: ', bio)
+
+    #  Find the user's website
+    website = soup.find('a', class_='u-url')
+    website = website['href'] if website else 'Not available'
+    print('Website: ', website)
 
     # Find the user's location
     location = soup.find('span', class_='p-label')
@@ -39,10 +48,21 @@ try:
     profile_image = profile_image['src'] if profile_image else 'Not available'
     print('Profile Image: ', profile_image)
 
+    #  Find social media links
+    social_links = soup.find_all('a', class_='u-url')
+    social_links = [link['href'] for link in social_links] if social_links else 'Not available'
+    print('Social Links: ', social_links)
+
     # Find the number of repositories
     repo = soup.find_all('span', class_='Counter')
     repos = repo[0].text if repo else 'Not available'
     print('Number of repositories: ', repos)
+
+    # Find the number of stars
+    stars = soup.find_all('span', class_='Counter')
+    stars = stars[2].text if stars else 'Not available'
+    print('Number of stars: ', stars)
+
 
 except requests.exceptions.RequestException as e:
     print('Error fetching data:', e)
